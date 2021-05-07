9f6b4edf74b0569521787832a42e2c0d564e3e04
AlirezaFereydoni <AlirezaFereydoni@outlook.com>
2021-05-07 21:23:01 +0430

    Updates

---
+++ precache-manifest.09a7e6cdecae5e89722a6c8cf06b2370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e802c3a66e4d75dfe6893a5c241efb",
+    "url": "/ToDo-Application/index.html"
+  },
+  {
+    "revision": "11f528970fb63d84bb96",
+    "url": "/ToDo-Application/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32ddfea53c5054a5092a",
+    "url": "/ToDo-Application/static/js/2.27aa2f0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-Application/static/js/2.27aa2f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f528970fb63d84bb96",
+    "url": "/ToDo-Application/static/js/main.ee3fecb4.chunk.js"
+  },
+  {
+    "revision": "f0894e26a5c4a1f804d6",
+    "url": "/ToDo-Application/static/js/runtime-main.9da17b9e.js"
+  }
+]);
